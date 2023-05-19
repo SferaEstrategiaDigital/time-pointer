631e3c9742d0f855c463c7ab96d16429c1fb8b6f
MarceloHFilho <contato@omarceleza.com.br>
2023-05-18 21:14:50 -0300

    Início do versionamento

---
+++ resources/js/Pages/Home/Acoes.vue
@@ -0,0 +1,30 @@
+<script lang="ts"></script>
+
+<template>
+    <div class="grid grid-cols-12 gap-4">
+        <a class="btn">
+            <p>Busca Avançada</p>
+        </a>
+        <a class="btn">
+            <p>Meus Imóveis Favoritos</p>
+        </a>
+        <a class="btn">
+            <p>Imóveis em disputa</p>
+        </a>
+        <a class="btn">
+            <p>Configurar alerta por e-mail</p>
+        </a>
+        <a class="btn">
+            <p>Assinar Plano Premium</p>
+        </a>
+    </div>
+</template>
+
+<style scoped>
+.btn {
+    @apply col-span-12 p-2 bg-orange-600/70 text-green-800 font-bold text-xl rounded-md md:col-span-4 md:text-center md:p-4;
+}
+.btn p {
+    @apply inline-block ml-2 md:block;
+}
+</style>
---
+++ resources/js/Pages/Home/Dashboard.vue
@@ -0,0 +1,27 @@
+<script lang="ts"></script>
+
+<template>
+    <div class="grid grid-cols-12 gap-4">
+        <a class="btn">
+            <p>89653</p>
+            <p>Imoveis</p>
+        </a>
+        <a class="btn">
+            <p>89653</p>
+            <p>Novos</p>
+        </a>
+        <a class="btn">
+            <p>4</p>
+            <p>Vendidos</p>
+        </a>
+    </div>
+</template>
+
+<style>
+.btn {
+    @apply col-span-12 bg-orange-600 text-green-800 font-bold text-xl rounded-md md:col-span-4 md:text-center md:p-4;
+}
+.btn p {
+    @apply inline-block ml-2 md:block;
+}
+</style>
