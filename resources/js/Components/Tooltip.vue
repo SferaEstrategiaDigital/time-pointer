84a15a199c21ec0eadcc73b6240d2710bb325c5c
MarceloHFilho <contato@omarceleza.com.br>
2023-06-25 18:02:54 -0300

    Mudanças no componente Tooltip

--- resources/js/Components/Tooltip.vue
+++ resources/js/Components/Tooltip.vue
@@ -1,13 +1,69 @@
 <template>
-    <div class="relative group">
-        <button><slot name="title"></slot></button>
+    <div
+        class="relative group"
+        ref="tooltipContainer"
+        @click="toggleTooltip"
+        @mouseenter="hoverTooltip"
+        @mouseleave="leaveTooltip"
+    >
+        <button ref="tooltipTrigger"><slot name="title"></slot></button>
         <div
-            class="absolute left-0 hidden group-hover:block transition ease-in-out duration-200"
+            ref="tooltipContent"
+            class="absolute left-0 w-full transition ease-in-out duration-200"
             style="top: 100%"
+            :class="{ hidden: !tooltipVisible }"
         >
             <slot name="content"></slot>
         </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+const tooltipContainer = ref(null);
+const tooltipTrigger = ref(null);
+const tooltipContent = ref(null);
+const tooltipVisible = ref(false);
+const props = defineProps({
+    clickToShow: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+onMounted(() => {
+    document.addEventListener("click", outsideClickListener);
+});
+
+onUnmounted(() => {
+    document.removeEventListener("click", outsideClickListener);
+});
+
+function toggleTooltip(event) {
+    if (props.clickToShow && !tooltipContent.value.contains(event.target)) {
+        tooltipVisible.value = !tooltipVisible.value;
+    }
+}
+
+function hoverTooltip() {
+    if (!props.clickToShow) {
+        tooltipVisible.value = true;
+    }
+}
+
+function leaveTooltip() {
+    if (!props.clickToShow) {
+        tooltipVisible.value = false;
+    }
+}
+
+function outsideClickListener(event) {
+    if (
+        !tooltipContainer.value.contains(event.target) &&
+        !tooltipContent.value.contains(event.target)
+    ) {
+        tooltipVisible.value = false;
+    }
+}
+</script>
