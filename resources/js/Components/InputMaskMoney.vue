777a2521df8eda27c2968e7336875c3eacd0c42e
MarceloHFilho <contato@omarceleza.com.br>
2023-06-25 18:22:40 -0300

    Add componente de gestão de Valores

---
+++ resources/js/Components/InputMaskMoney.vue
@@ -0,0 +1,23 @@
+<template>
+    <input type="text" :placeholder="placeholder" v-model="inputValue" />
+</template>
+
+<script setup>
+import { ref, watch, defineProps } from "vue";
+
+const props = defineProps({
+    placeholder: {
+        type: String,
+        default: "",
+    },
+});
+
+let inputValue = ref("");
+
+watch(inputValue, (newVal, oldVal) => {
+    let regex = /^\d*,?\d*$/;
+    if (!regex.test(newVal) || (newVal.match(/,/g) || []).length > 1) {
+        inputValue.value = oldVal;
+    }
+});
+</script>
