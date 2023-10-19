8983c9a44ce1e436502547f5eff1a2f80171e2ef
MarceloHFilho <contato@omarceleza.com.br>
2023-10-19 20:05:20 -0300

    Add controller e endpoint para busca de Imoveis

---
+++ app/Http/Controllers/ImovelsController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Imovel;
+use Illuminate\Http\Request;
+
+class ImovelsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $imoveis = Imovel::search($query)->get();
+        // return view('search-results', ['imoveis' => $imoveis]);
+        return response()->json(['imoveis' => $imoveis]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Imovel $imovel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Imovel $imovel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Imovel $imovel)
+    {
+        //
+    }
+}
